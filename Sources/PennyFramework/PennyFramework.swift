9a4906b9877b0e128458aacdc872339ecc9fa5f4
clarknt <24315769+clarknt@users.noreply.github.com>
2020-05-20 18:48:26 -0400

    Add new new class and unit test

--- Sources/PennyFramework/PennyFramework.swift
+++ Sources/PennyFramework/PennyFramework.swift
@@ -1,3 +1,28 @@
-struct PennyFramework {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct PennyFramework {
+    public struct MainTextField: View {
+        @State var placeholder: String
+        @Binding var text: String
+
+        public init(placeholder: String, text: Binding<String>) {
+            self._placeholder = State(initialValue: placeholder)
+            self._text = text
+        }
+
+        public var body: some View {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.blue)
+                TextField(placeholder, text: $text)
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(.blue)
+            }
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.blue, lineWidth: 2)
+            )
+        }
+    }
 }
--- Tests/PennyFrameworkTests/PennyFrameworkTests.swift
+++ Tests/PennyFrameworkTests/PennyFrameworkTests.swift
@@ -1,12 +1,19 @@
 import XCTest
+import SwiftUI
 @testable import PennyFramework
 
 final class PennyFrameworkTests: XCTestCase {
+    @State var text: String
+
+    public init(text: String) {
+        self.text = text
+        super.init()
+    }
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PennyFramework().text, "Hello, World!")
+        var body: some View {
+            PennyFramework.MainTextField(placeholder: "Testing", text: $text)
+        }
     }
 
     static var allTests = [
